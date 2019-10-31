e7b756724af93f4cfb8f2b0c700eac49a7f09790
Josh Hall <joshhall90@yahoo.com>
2019-10-30 21:51:57 -0400

    Testing to make heroku app load

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/header';
-import { Switch, Route } from 'react-router-dom';
-import Home from './views/home';
-import Shop from './views/shop';
-// import products from './products.js';
-import Cart from './views/cart';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/header";
+import { Switch, Route } from "react-router-dom";
+import Home from "./views/home";
+import Shop from "./views/shop";
+import Cart from "./views/cart";
 // import {Elements, StripeProvider} from 'react-stripe-elements';
 // import CheckoutForm from './views/checkoutForm';
 
 class App extends Component {
   constructor() {
     super();
-    this.state= {
-      'products': [],
-      'cart': [],
-      'total': 0
-    }
+    this.state = {
+      products: [],
+      cart: [],
+      total: 0
+    };
 
     this.calcTotal = this.calcTotal.bind(this);
   }
 
   componentWillMount() {
     this.getProducts();
-    this.setState({ 'cart': JSON.parse(sessionStorage.getItem('cart')) });
+    this.setState({ cart: JSON.parse(sessionStorage.getItem("cart")) });
     this.calcTotal();
   }
 
   countDuplicates = (id, size) => {
-    let cart = JSON.parse(sessionStorage.getItem('cart'));
+    let cart = JSON.parse(sessionStorage.getItem("cart"));
 
     let count = 0;
     for (let i in cart) {
@@ -36,37 +35,41 @@ class App extends Component {
         count += 1;
       }
     }
-    return count
-  }
+    return count;
+  };
 
   addItem = id => {
-    if (sessionStorage.getItem('cart')) {
-      var cart = JSON.parse(sessionStorage.getItem('cart'));
-    }
-    else {
+    if (sessionStorage.getItem("cart")) {
+      var cart = JSON.parse(sessionStorage.getItem("cart"));
+    } else {
       cart = [];
     }
 
     for (let i in this.state.products) {
-      if(this.state.products[i].product_id === id) {
-
+      if (this.state.products[i].product_id === id) {
         // Get the selected size option
-        let sizes = document.getElementById("sizes" + this.state.products[i].product_id);
+        let sizes = document.getElementById(
+          "sizes" + this.state.products[i].product_id
+        );
         let size = sizes.selectedOptions[0].label;
         // console.log(size)
 
         // Update quantity
-        let count = this.countDuplicates(id,size);
+        let count = this.countDuplicates(id, size);
 
         if (count <= 0) {
           cart.push({
-            'product': this.state.products[i],
-            'size': size,
-            'quantity': 1
+            product: this.state.products[i],
+            size: size,
+            quantity: 1
           });
         } else {
           for (let j in cart) {
-            if (cart[j].size === size && cart[j].product.product_id === id && count >= 1) {
+            if (
+              cart[j].size === size &&
+              cart[j].product.product_id === id &&
+              count >= 1
+            ) {
               cart[j].quantity += 1;
               break;
             }
@@ -77,20 +80,20 @@ class App extends Component {
       }
     }
 
-    console.log(cart)
+    console.log(cart);
 
-    sessionStorage.setItem('cart', JSON.stringify(cart));
+    sessionStorage.setItem("cart", JSON.stringify(cart));
 
     this.calcTotal();
-    this.setState({'cart': cart});
-  }
+    this.setState({ cart: cart });
+  };
 
-  addCartItem = (id,size) => {
+  addCartItem = (id, size) => {
     // get cart key from sessionStorage and parse it into object
-    let cart = JSON.parse(sessionStorage.getItem('cart'));
+    let cart = JSON.parse(sessionStorage.getItem("cart"));
 
     for (let i in cart) {
-      if(cart[i].product.product_id === id && cart[i].size === size) {
+      if (cart[i].product.product_id === id && cart[i].size === size) {
         if (cart[i].quantity >= 1) {
           cart[i].quantity += 1;
           break;
@@ -98,18 +101,18 @@ class App extends Component {
       }
     }
 
-    sessionStorage.setItem('cart',JSON.stringify(cart));
+    sessionStorage.setItem("cart", JSON.stringify(cart));
 
     this.calcTotal();
-    this.setState({'cart': cart});
-  }
+    this.setState({ cart: cart });
+  };
 
-  removeItem = (id,size) => {
+  removeItem = (id, size) => {
     // get cart key from sessionStorage and parse it into object
-    let cart = JSON.parse(sessionStorage.getItem('cart'));
+    let cart = JSON.parse(sessionStorage.getItem("cart"));
 
     for (let i in cart) {
-      if(cart[i].product.product_id === id && cart[i].size === size) {
+      if (cart[i].product.product_id === id && cart[i].size === size) {
         if (cart[i].quantity > 1) {
           cart[i].quantity -= 1;
           break;
@@ -120,37 +123,36 @@ class App extends Component {
       }
     }
 
-    sessionStorage.setItem('cart',JSON.stringify(cart));
+    sessionStorage.setItem("cart", JSON.stringify(cart));
 
     this.calcTotal();
-    this.setState({'cart': cart});
-
-  }
+    this.setState({ cart: cart });
+  };
 
   // calculating and returning the Total
   calcTotal() {
     // get value and parse sessionStorage
-    let cart = JSON.parse(sessionStorage.getItem('cart'));
+    let cart = JSON.parse(sessionStorage.getItem("cart"));
 
     // use state before changing to session storage
     // let cart = this.state.cart;
     let total = 0;
 
     for (let product in cart) {
-      total += (cart[product].product.price * cart[product].quantity);
+      total += cart[product].product.price * cart[product].quantity;
     }
 
     total = total.toFixed(2);
 
-    this.setState({'total': total});
+    this.setState({ total: total });
   }
 
-  getFiltered = async() => {
-    let url = 'https://snow-gear-shop-backend.herokuapp.com/api/filter';
+  getFiltered = async () => {
+    let url = "https://snow-gear-shop-backend.herokuapp.com/api/filter";
 
     let response = await fetch(url, {
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
     });
 
@@ -158,15 +160,14 @@ class App extends Component {
 
     // if the variable you are saving into the state is the same, yu cane just use the code below instead of events: events
     this.setState({ products });
-  }
+  };
 
-
-  getProducts = async() => {
-    let url = 'https://snow-gear-shop-backend.herokuapp.com/api/retrieve';
+  getProducts = async () => {
+    let url = "https://snow-gear-shop-backend.herokuapp.com/api/retrieve";
 
     let response = await fetch(url, {
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
     });
 
@@ -174,18 +175,38 @@ class App extends Component {
 
     // if the variable you are saving into the state is the same, you can just use the code below instead of events: events
     this.setState({ products });
-  }
-
+  };
 
   render() {
     return (
       <div className="App">
-        <Header total={this.state.total}/>
+        <Header total={this.state.total} />
 
         <Switch>
-          <Route exact path='/' render={() => <Home products={this.state.products}/>} />
-          <Route exact path='/shop' render={() => <Shop products={this.state.products} addItem={this.addItem}/>} />
-          <Route exact path='/cart' render={() => <Cart cart={this.state.cart} total={this.state.total} removeItem={this.removeItem} addCartItem={this.addCartItem}/>} />
+          <Route
+            exact
+            path="/"
+            render={() => <Home products={this.state.products} />}
+          />
+          <Route
+            exact
+            path="/shop"
+            render={() => (
+              <Shop products={this.state.products} addItem={this.addItem} />
+            )}
+          />
+          <Route
+            exact
+            path="/cart"
+            render={() => (
+              <Cart
+                cart={this.state.cart}
+                total={this.state.total}
+                removeItem={this.removeItem}
+                addCartItem={this.addCartItem}
+              />
+            )}
+          />
         </Switch>
       </div>
     );
--- src/components/productItem/index.js
+++ src/components/productItem/index.js
@@ -1,56 +1,69 @@
-import React, { Component } from 'react';
-import './index.css';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import boot from '../../media/images/snowboard_boots.jpg';
-import glove from '../../media/images/snowboard_gloves.jpg';
-import mitten from '../../media/images/snowboard_mittens.jpg';
-import pants from '../../media/images/snowboard_pants.jpg';
-import jacket from '../../media/images/snowboard-jacket1.jpg';
-import snowboard from '../../media/images/snowboard-snowboard1.jpg';
+import React, { Component } from "react";
+import "./index.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import boot from "../../media/images/snowboard_boots.jpg";
+import glove from "../../media/images/snowboard_gloves.jpg";
+import mitten from "../../media/images/snowboard_mittens.jpg";
+import pants from "../../media/images/snowboard_pants.jpg";
+import jacket from "../../media/images/snowboard-jacket1.jpg";
+import snowboard from "../../media/images/snowboard-snowboard1.jpg";
 
 class ProductItem extends Component {
-
-
   render() {
     return (
-      <div className="row">
-        <div className="col-md-12">
+      <div className=" shop-container">
+        <div className="col-md-12 shop-grid">
           {this.props.products &&
-            this.props.products.map( product =>
-              <Card className="col-md-4 inLine hov" id={product.product_id} >
+            this.props.products.map(product => (
+              <Card className="col-md-4 inLine hov" id={product.product_id}>
                 <Card.Body>
-                  {product.genre == 'boots' &&
+                  {product.genre === "boots" && (
                     <Card.Img variant="top" src={boot} />
-                  }
-                  {product.genre == 'snowboard' &&
+                  )}
+                  {product.genre === "snowboard" && (
                     <Card.Img variant="top" src={snowboard} />
-                  }
-                  {product.genre == 'jacket' &&
+                  )}
+                  {product.genre === "jacket" && (
                     <Card.Img variant="top" src={jacket} />
-                  }
-                  {product.genre == 'pants' &&
+                  )}
+                  {product.genre === "pants" && (
                     <Card.Img variant="top" src={pants} />
-                  }
-                  {product.genre == 'gloves' &&
+                  )}
+                  {product.genre === "gloves" && (
                     <Card.Img variant="top" src={glove} />
-                  }
-                  {product.genre == 'mittens' &&
+                  )}
+                  {product.genre === "mittens" && (
                     <Card.Img variant="top" src={mitten} />
-                  }
-                  <Card.Header className="" as="h5">{ product.name }</Card.Header>
-                  <Card.Subtitle className="mb-2 text-muted">${ product.price }</Card.Subtitle>
-                  <Card.Text className="">{ product.desc }</Card.Text>
-                  <select className="card-select" id={'sizes' + product.product_id}>
-                    {product.sizes.map( size =>
-                      <option key={'size-' + product.product_id + '-' + size}>{size}</option>
-                     )}
+                  )}
+                  <Card.Header className="" as="h5">
+                    {product.name}
+                  </Card.Header>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    ${product.price}
+                  </Card.Subtitle>
+                  <Card.Text className="">{product.desc}</Card.Text>
+                  <select
+                    className="card-select"
+                    id={"sizes" + product.product_id}
+                  >
+                    {product.sizes.map(size => (
+                      <option key={"size-" + product.product_id + "-" + size}>
+                        {size}
+                      </option>
+                    ))}
                   </select>
-                  <Button className="" variant="primary" onClick={() => this.props.addItem(product.product_id)}>Add to Cart</Button>
-                </Card.Body> {/* ends card */}
+                  <Button
+                    className=""
+                    variant="primary"
+                    onClick={() => this.props.addItem(product.product_id)}
+                  >
+                    Add to Cart
+                  </Button>
+                </Card.Body>{" "}
+                {/* ends card */}
               </Card>
-            )
-          }
+            ))}
         </div>
       </div>
     );
@@ -59,8 +72,6 @@ class ProductItem extends Component {
 
 export default ProductItem;
 // <Card.Img variant="top" src="https://via.placeholder.com/100x60.png" />
-
-
 
 // className="col-md-4 inLine"
 // <Card style={{ width: '18rem' }}>
